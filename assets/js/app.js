61fb5db3293acc06329a73effdb59fa4e3f04426
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2020-08-06 16:26:59 -0300

    Função de mostrar e esconder senha

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-console.log('ìs us dog')
+
+function show(){
+    if(senha.type == 'password'){
+        senha.type = 'text';
+
+        show_block.classList.remove('fa-eye');
+        show_block.classList.add('fa-eye-slash');
+    }else{
+        senha.type = 'password';
+
+        show_block.classList.add('fa-eye');
+        show_block.classList.remove('fa-eye-slash');
+    }
+}
+
+function validation(){
+    console.log('ìs us dog')
+}
