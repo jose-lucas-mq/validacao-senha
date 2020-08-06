ecde6abdb9bd75f4646478f3180736f23a615d65
jose-lucas-mq <jose.queiroz@bandtec.com.br>
2020-08-06 16:52:19 -0300

    Fazendo validação de senha e mostrando botão se ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 
-function show(){
-    if(senha.type == 'password'){
+function show() {
+    if (senha.type == 'password') {
         senha.type = 'text';
 
         show_block.classList.remove('fa-eye');
         show_block.classList.add('fa-eye-slash');
-    }else{
+    } else {
         senha.type = 'password';
 
         show_block.classList.add('fa-eye');
@@ -13,6 +13,43 @@ function show(){
     }
 }
 
-function validation(){
-    console.log('ìs us dog')
+function validation() {
+    let minha_senha = senha.value;
+    let cont = 0;
+    // MINUSCULAS
+    if (minha_senha.match(/[a-z]+/)) {
+        minusculas.classList.remove('fa-times');
+        minusculas.classList.add('fa-check');
+
+        ++cont;
+
+    }
+
+    // MAIUSCULAS
+    if (minha_senha.match(/[A-Z]+/)) {
+        maiusculas.classList.remove('fa-times');
+        maiusculas.classList.add('fa-check');
+
+        ++cont;
+    }
+
+    //NUMEROS
+    if (minha_senha.match(/\d/)) {
+        numeros.classList.remove('fa-times');
+        numeros.classList.add('fa-check');
+
+        ++cont;
+    }
+
+    // SIMBOLOS
+    if (minha_senha.match(/\W/)) {
+        simbolos.classList.remove('fa-times');
+        simbolos.classList.add('fa-check');
+
+        ++cont;
+    }
+
+    if(cont >= 4){
+        ir.removeAttribute("disabled");
+    }
 }
